024b9c94a157e68561be76cfc8157e6f8ac7a21f
Ivan TANKOUA <itankoua@software-phone.com>
2021-10-13 23:18:07 +0200

    first

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ telnet.createServer(function (client) {
   // make unicode characters work properly
   client.do.transmit_binary()
  
-  // make the client emit 'window size' events
-  client.do.window_size()
- 
-  // listen for the window size events from the client
-  client.on('window size', function (e) {
-    if (e.command === 'sb') {
-      console.log('telnet window resized to %d x %d', e.width, e.height)
-    }
-  })
  
   // listen for the actual data from the client
   client.on('data', function (b) {

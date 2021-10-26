3ba15a3ab661504a006c496a2b0fb0cdbc391141
Ivan TANKOUA <itankoua@software-phone.com>
2021-10-26 21:41:14 +0200

    server

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ telnet.createServer(function (client) {
   // make unicode characters work properly
   client.do.transmit_binary()
  
- 
   // listen for the actual data from the client
   client.on('data', function (b) {
-    client.write(b)
+    var cmd = b.toString()
+    var result = "OK \n"
+    if(cmd === "VER\r") {
+      result += "VERSION: \n" 
+      result += "FD42140197 V:001.001 \n"
+    } else if(cmd === "BYE\r") {
+      result += "BYE BYE \r"
+    } else if (cmd.includes("CFG 0 13")) {
+      result += "Extended Code Set \n"
+    }
+    client.write(result)
+    console.log(cmd)
+    console.log(result)
+    console.log("-----------\n")
   })
- 
-  client.write('connected to Telnet server!')
- 
-}).listen(23)
+}).listen(2300)
 
